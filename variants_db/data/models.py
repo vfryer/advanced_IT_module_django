53464c24a87ec4cb6b507ae94b12feef47cc4efa
vfryer <verity.fryer@nhs.net>
2018-03-01 12:14:41 +0000

    Rename model

--- variants_db/data/models.py
+++ variants_db/data/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Raw_data(models.Model):
+class Sample(models.Model):
     sample_number = models.IntegerField('Sample_Number')
     first_name = models.CharField('First_Name', max_length=100)
     last_name = models.CharField('Last_Name', max_length=100)
@@ -11,3 +11,6 @@ class Raw_data(models.Model):
     cDNA_variant = models.CharField('cDNA_change', max_length=200)
     protein_variant = models.CharField('Protein_change', max_length=200)
     genomic_variant = models.CharField('Genome_change', max_length=200)
+
+    def __str__(self):
+        return self.cDNA_variant + ', ' + protein_variant
