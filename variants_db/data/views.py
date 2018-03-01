57e13026e91e65fd1daddc3dddabefcc02e9bf13
vfryer <verity.fryer@nhs.net>
2018-03-01 16:06:08 +0000

    Adding new view

--- variants_db/data/views.py
+++ variants_db/data/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import HttpResponse
 
+from data.models import Sample
+
 def index(request):
-    return HttpResponse("This is going to be a variant database. Work in Progress")
+    list_of_patient_names = Sample.objects.values()
+    return HttpResponse(list_of_patient_names)
+#    return HttpResponse("This is going to be a variant database. Work in Progress")
