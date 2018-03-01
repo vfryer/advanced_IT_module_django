7d4d18913944eb256036510a3f039e54bebcc1fb
vfryer <verity.fryer@nhs.net>
2018-03-01 11:16:17 +0000

    Clear and re-establish database with Django models

--- variants_db/variants_db/settings.py
+++ variants_db/variants_db/settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'data.apps.DataConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
